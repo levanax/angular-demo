0dea107e447231b1adfc6c1d4398f64a718adae1
unknown <Levana.Xue@SZRND28.2gotradesz.com.cn>
2015-06-15 14:44:18 +0800

    更新项目结构，试用 ng-include

---
+++ app/scripts/config/constants.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('portalDemoApp')
+.constant('cookie',{
+	life:30
+});
---
+++ app/scripts/controllers/commons/footCtrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('portalDemoApp')
+.controller('footCtrl',['$scope',function($scope){
+	$scope.targetLink = 'https://code.angularjs.org/1.3.16/docs/api/ng/directive/ngInclude';
+}]);
